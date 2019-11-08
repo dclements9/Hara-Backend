8e36acaff16b55603fd7243b32674e9fcee315c8
dclements9 <dclements@protonmail.com>
2019-11-08 11:01:24 -0500

    Students & lessons controller, routes, db, & seeds

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,48 @@
+class LessonsController < ApplicationController
+    before_action :set_lesson, only: [:show, :update, :destroy]
+
+    def index
+      @lessons = Lesson.all
+  
+      render json: @lessons
+    end
+  
+    def show
+      render json: @lesson
+    end
+  
+    def create
+      @lesson = Lesson.new(lesson_params)
+  
+      if @lesson.save
+        render json: @lesson, status: :created, location: @lesson
+      else
+        render json: @lesson.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /lessons/1
+    def update
+      if @lesson.update(lesson_params)
+        render json: @lesson
+      else
+        render json: @lesson.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /lessons/1
+    def destroy
+      @lesson.destroy
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_lesson
+        @lesson = Lesson.find(params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def lesson_params
+        params.require(:lesson).permit(:title, :description, :date, :start_time, :end_time)
+      end
+end
