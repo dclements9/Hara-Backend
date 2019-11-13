a103fbaa607eb8f250ca29d860976952f731f88f
dclements9 <dclements@protonmail.com>
2019-11-12 19:30:50 -0500

    auth controller & route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show, :create, :update, :destroy]
   resources :lessons, only: [:index, :show, :create, :update, :destroy]
+  post '/login', to: 'auth#login'
+  
 end
