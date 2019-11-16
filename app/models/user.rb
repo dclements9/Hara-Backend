c39f178888b1f70b43297cc4df3bdf566e964bfe
Dylan Clements <dclements@protonmail.com>
2019-11-16 08:25:26 -0500

    Changed user validations while StudentPortal is WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
 
-  validates :email, :password, :first_name, :last_name, presence: true
+#  TODO: Add password_digest to password
+#   Removed before StudentPortal is ready
+
+  validates :email, :password_digest, :first_name, :last_name, presence: true
   validates :email, uniqueness: true
-  validates :password, length: { minimum: 8 }
+  # validates :password, length: { minimum: 8 }
 end
