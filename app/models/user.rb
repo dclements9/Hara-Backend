f6d1d9706cc0e8017761fdcff64091f703d367a7
dclements9 <dclements@protonmail.com>
2019-11-16 15:31:32 -0500

    temp removed password_digest validation & has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
-    has_secure_password
+    # has_secure_password
 
 #  TODO: Add password_digest to password
 #   Removed before StudentPortal is ready
 
-  validates :email, :password_digest, :first_name, :last_name, presence: true
+#   validates :email, :password_digest, :first_name, :last_name, presence: true
+  validates :email, :first_name, :last_name, presence: true
   validates :email, uniqueness: true
   # validates :password, length: { minimum: 8 }
 end
