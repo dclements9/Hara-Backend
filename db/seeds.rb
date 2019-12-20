ad4805db2f63f8e9dc28e519e211a9d4a084b311
Dylan Clements <dclements@protonmail.com>
2019-12-20 09:10:45 -0500

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,15 +17,16 @@ JaneDoe = User.create(first_name: 'Jane', last_name: 'Doe', email: 'jdoe@doe.com
     password_digest: 'password', age: 24, rank: "8th kup")
 
 # Lessons
-BeginnerLesson = Lesson.create(title: 'Beginner Class', description: "The Beginner Class for all.", date: "2019-12-01", start_time: "2000-01-01 08:00:00", end_time: "2000-01-01 09:00:00")
+BeginnerLesson = Lesson.create(title: 'Beginner Class', description: "The Beginner Class for all.", date: "2019-12-20", start_time: "2000-01-01 08:00:00", end_time: "2000-01-01 09:00:00")
 BeginnerLesson.users = [JohnSmith, JaneDoe]
 
-IntermediateLesson = Lesson.create(title: 'Intermediate Class', description: "The Intermediate Class for the more advanced.", date: "2019-12-01", start_time: "2000-01-01 09:00:00", end_time: "2000-01-01 10:00:00")
+IntermediateLesson = Lesson.create(title: 'Intermediate Class', description: "The Intermediate Class for the more advanced.", date: "2019-12-20", start_time: "2000-01-01 09:00:00", end_time: "2000-01-01 10:00:00")
 IntermediateLesson.users = [JoeShmo]
 
-AdvancedLesson = Lesson.create(title: 'Advanced Class', description: "The Advanced Class for the properly skilled.", date: "2019-12-01", start_time: "2000-01-01 10:00:00", end_time: "2000-01-01 01:00:00")
+AdvancedLesson = Lesson.create(title: 'Advanced Class', description: "The Advanced Class for the properly skilled.", date: "2019-12-20", start_time: "2000-01-01 10:00:00", end_time: "2000-01-01 11:00:00")
+AdvancedLesson.users = [JoeShmo]
 
-ExtraLesson = Lesson.create(title: 'Extra Class', description: "The Extra Class for more fun.", date: "2019-12-03", start_time: "2000-01-01 10:00:00", end_time: "2000-01-01 01:00:00")
+ExtraLesson = Lesson.create(title: 'Extra Class', description: "The Extra Class for more fun.", date: "2019-12-09", start_time: "2000-01-01 10:00:00", end_time: "2000-01-01 01:00:00")
 
 # Challenges
 FirstWeeklyChallenge = Challenge.create(title: 'First Weekly Challenge', description: 'This is the First Weekly Challenge', 
